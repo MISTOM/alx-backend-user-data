7042a7d3181dc051c5984153e14f379e37d5a847
MISTOM <kigardetom2001@gmail.com>
2022-05-13 21:30:53 +0300

    pep8 pycodestyle

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -95,5 +95,6 @@ class RedactingFormatter(logging.Formatter):
         txt = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
         return txt
 
+
 if __name__ == "__main__":
     main()
