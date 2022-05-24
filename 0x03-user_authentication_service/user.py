6b5e8e1705ae45134d71ed36f8ce0b099ef9c6ae
MISTOM <kigardetom2001@gmail.com>
2022-05-24 12:36:02 +0300

    fix

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 """The user model's module
 """
-
-
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -11,11 +9,11 @@ Base = declarative_base()
 
 
 class User(Base):
-    """Represents a record from the `User` table
+    """Represents a record from the `user` table.
     """
-    __tablename__ = "Users"
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=False)
-    reset_token = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
