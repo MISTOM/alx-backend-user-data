f8a549cc31de968dd7ed96a981607c82983d319a
MISTOM <kigardetom2001@gmail.com>
2022-05-24 12:29:14 +0300

    assert the app

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -109,3 +109,15 @@ def update_password(email: str, reset_token: str, new_password: str) -> None:
     res = requests.put(url, data=body)
     assert res.status_code == 200
     assert res.json() == {"email": email, "message": "Password updated"}
+
+
+if __name__ == "__main__":
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
